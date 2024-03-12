3215d5a893bff25fb04a5be51131fc6bd8c7024d
Mac-usb <mac@MacBook-Pro.local>
2024-03-11 21:28:59 -0500

    Modificacion Despacho Entrega en PDF

--- routers/generatePdf.go
+++ routers/generatePdf.go
@@ -26,7 +26,6 @@ func GeneratePdf(w http.ResponseWriter, r *http.Request) {
 
 	pdf := gofpdf.New("P", "mm", "A4", "")
 
-	
 	encabezado := func() {
 		RespDespacho, err := bd.GetDespachosbyID(IDDespacho)
 		if err != nil {
@@ -34,7 +33,7 @@ func GeneratePdf(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		// pdf.Image("./assets/logo.png", 120, 7, 15, 0, false, "", 0, "http://www.honesolutionst.com")
-		
+
 		pdf.AddUTF8Font("dejavu", "", "./font/DejaVuSansCondensed.ttf")
 		pdf.AddUTF8Font("dejavu", "B", "./font/DejaVuSansCondensed-Bold.ttf")
 		pdf.AddUTF8Font("dejavu", "I", "./font/DejaVuSansCondensed-Oblique.ttf")
@@ -46,32 +45,32 @@ func GeneratePdf(w http.ResponseWriter, r *http.Request) {
 		html.Write(lineHt, htmlStr)
 		pdf.SetLeftMargin(80)
 		pdf.SetFontSize(13)
-			pdf.Ln(-1)
-			pdf.CellFormat(45, 6, "TEXTILES SPIRIT S.A.S.: ", "0", 0, "", false, 0, "")
-			pdf.Ln(-1)
-			pdf.CellFormat(45, 6, "NIT 901208883-1", "0", 0, "", false, 0, "")
-			pdf.Ln(-1)
-			pdf.CellFormat(45, 6, "CALLE 78 A # 63 - 64", "0", 0, "", false, 0, "")
-			pdf.SetLeftMargin(10)
-			pdf.SetFontSize(11)
-		
+		pdf.Ln(-1)
+		pdf.CellFormat(45, 6, "TEXTILES SPIRIT S.A.S.: ", "0", 0, "", false, 0, "")
+		pdf.Ln(-1)
+		pdf.CellFormat(45, 6, "NIT 901208883-1", "0", 0, "", false, 0, "")
+		pdf.Ln(-1)
+		pdf.CellFormat(45, 6, "CALLE 78 A # 63 - 64", "0", 0, "", false, 0, "")
+		pdf.SetLeftMargin(10)
+		pdf.SetFontSize(11)
+
 		pdf.Ln(10)
-		
-			pdf.CellFormat(100, 6, "Cliente: "+fmt.Sprintf("%v", RespDespacho[0].Tercero.Name), "0", 0, "", false, 0, "")
-			pdf.CellFormat(40, 6, fmt.Sprintf("%v",""), "0", 0, "", false, 0, "")
-			pdf.CellFormat(100, 6, fmt.Sprintf("%v",""), "0", 0, "", false, 0, "")
-			pdf.Ln(-1)
-			pdf.CellFormat(60, 6, "Departamento: "+fmt.Sprintf("%v", RespDespacho[0].Tercero.State), "0", 0, "", false, 0, "")
-			pdf.CellFormat(60, 6, "Ciudad: "+fmt.Sprintf("%v", RespDespacho[0].Tercero.City), "0", 0, "", false, 0, "")
-			pdf.CellFormat(120, 6, "Dirección: "+fmt.Sprintf("%v", RespDespacho[0].Tercero.Adress), "0", 0, "", false, 0, "")
-			pdf.Ln(-1)
-			pdf.CellFormat(100, 6, "Teléfono " + fmt.Sprintf("%v",  RespDespacho[0].Tercero.Phone), "0", 0, "", false, 0, "")
-			pdf.CellFormat(40, 6, fmt.Sprintf("%v",""), "0", 0, "", false, 0, "")
-			pdf.CellFormat(100, 6, "Fecha " + fmt.Sprintf("%v",  RespDespacho[0].FechaDespacho), "0", 0, "", false, 0, "")
-			pdf.SetLeftMargin(10)
-			pdf.Ln(-1)
-			pdf.Ln(-1)
-		
+
+		pdf.CellFormat(100, 6, "Cliente: "+fmt.Sprintf("%v", RespDespacho[0].Tercero.Name), "0", 0, "", false, 0, "")
+		pdf.CellFormat(40, 6, fmt.Sprintf("%v", ""), "0", 0, "", false, 0, "")
+		pdf.CellFormat(100, 6, fmt.Sprintf("%v", ""), "0", 0, "", false, 0, "")
+		pdf.Ln(-1)
+		pdf.CellFormat(60, 6, "Departamento: "+fmt.Sprintf("%v", RespDespacho[0].Tercero.State), "0", 0, "", false, 0, "")
+		pdf.CellFormat(60, 6, "Ciudad: "+fmt.Sprintf("%v", RespDespacho[0].Tercero.City), "0", 0, "", false, 0, "")
+		pdf.CellFormat(120, 6, "Dirección: "+fmt.Sprintf("%v", RespDespacho[0].Tercero.Adress), "0", 0, "", false, 0, "")
+		pdf.Ln(-1)
+		pdf.CellFormat(100, 6, "Teléfono "+fmt.Sprintf("%v", RespDespacho[0].Tercero.Phone), "0", 0, "", false, 0, "")
+		pdf.CellFormat(40, 6, fmt.Sprintf("%v", ""), "0", 0, "", false, 0, "")
+		pdf.CellFormat(100, 6, "Fecha "+fmt.Sprintf("%v", RespDespacho[0].FechaDespacho), "0", 0, "", false, 0, "")
+		pdf.SetLeftMargin(10)
+		pdf.Ln(-1)
+		pdf.Ln(-1)
+
 	}
 	basicTable := func() {
 		RespSalida, err := bd.GetSalidasbyDespacho(IDDespacho)
@@ -79,59 +78,64 @@ func GeneratePdf(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Error el leer el despacho", http.StatusBadRequest)
 			return
 		}
-		
+		RespDespacho, err := bd.GetDespachosbyID(IDDespacho)
+		if err != nil {
+			http.Error(w, "Error el leer el despacho por id", http.StatusBadRequest)
+			return
+		}
+
 		pdf.SetFillColor(224, 235, 255)
-		
+
 		fill := false
 		pdf.CellFormat(10, 6, "No", "1", 0, "", fill, 0, "")
-			pdf.CellFormat(50, 6, "Código", "1", 0, "", fill, 0, "")
-			pdf.CellFormat(20, 6, "Metros", "1", 0, "", fill, 0, "")
-			pdf.CellFormat(80, 6, "Producto", "1", 0, "", fill, 0, "")
-			pdf.CellFormat(30, 6, "Referencia", "1", 0, "", fill, 0, "")
+		pdf.CellFormat(50, 6, "Código", "1", 0, "", fill, 0, "")
+		pdf.CellFormat(20, 6, "Metros", "1", 0, "", fill, 0, "")
+		pdf.CellFormat(80, 6, "Producto", "1", 0, "", fill, 0, "")
+		pdf.CellFormat(30, 6, "Referencia", "1", 0, "", fill, 0, "")
 		pdf.Ln(-1)
-		x:=1
-		 m := 0.0
+		x := 1
+		m := 0.0
 		for _, c := range RespSalida {
-			
-			
+
 			pdf.CellFormat(10, 6, fmt.Sprintf("%v", x), "1", 0, "", fill, 0, "")
 			pdf.CellFormat(50, 6, fmt.Sprintf("%v", c.Pieza.Codigo), "1", 0, "", fill, 0, "")
 			pdf.CellFormat(20, 6, fmt.Sprintf("%.2f", c.Pieza.MetrosRecibidos), "1", 0, "", fill, 0, "")
 			pdf.CellFormat(80, 6, c.Pieza.Producto.Name, "1", 0, "", fill, 0, "")
 			pdf.CellFormat(30, 6, c.Pieza.Producto.Code, "1", 0, "", fill, 0, "")
 			pdf.Ln(-1)
-			m=m+float64(c.Pieza.MetrosRecibidos)
+			m = m + float64(c.Pieza.MetrosRecibidos)
 			x++
 		}
-		x=x-1
+		x = x - 1
 		pdf.Ln(-1)
-		pdf.CellFormat(45, 6, "Total Piezas: "+fmt.Sprintf("%v",x), "0", 0, "", false, 0, "")
+		pdf.CellFormat(45, 6, "Total Piezas: "+fmt.Sprintf("%v", x), "0", 0, "", false, 0, "")
 		pdf.Ln(-1)
-			pdf.CellFormat(40, 6, "Total Metros: " +fmt.Sprintf("%.2f", m), "0", 0, "", false, 0, "")
-			pdf.Ln(-1)
-			pdf.Ln(-1)
-			pdf.Ln(-1)
-			pdf.Ln(-1)
-			pdf.Ln(-1)
-			pdf.CellFormat(45, 6, "___________________", "0", 0, "", false, 0, "")
-			pdf.CellFormat(45, 6, "", "0", 0, "", false, 0, "")
-			pdf.CellFormat(45, 6, "___________________", "0", 0, "", false, 0, "")
-			pdf.Ln(-1)
-			pdf.CellFormat(45, 6, "FIRMA RECIBIDO", "0", 0, "", false, 0, "")
-			pdf.CellFormat(45, 6, "", "0", 0, "", false, 0, "")
-			pdf.CellFormat(45, 6, "      FIRMA", "0", 0, "", false, 0, "")
-		
+		pdf.CellFormat(40, 6, "Total Metros: "+fmt.Sprintf("%.2f", m), "0", 0, "", false, 0, "")
+		pdf.CellFormat(120, 6, "Dirección Entrega: "+fmt.Sprintf("%v", RespDespacho[0].DireccionEntrega), "0", 0, "", false, 0, "")
+		pdf.Ln(-1)
+		pdf.Ln(-1)
+		pdf.Ln(-1)
+		pdf.Ln(-1)
+		pdf.Ln(-1)
+		pdf.CellFormat(45, 6, "___________________", "0", 0, "", false, 0, "")
+		pdf.CellFormat(45, 6, "", "0", 0, "", false, 0, "")
+		pdf.CellFormat(45, 6, "___________________", "0", 0, "", false, 0, "")
+		pdf.Ln(-1)
+		pdf.CellFormat(45, 6, "FIRMA RECIBIDO", "0", 0, "", false, 0, "")
+		pdf.CellFormat(45, 6, "", "0", 0, "", false, 0, "")
+		pdf.CellFormat(45, 6, "      FIRMA", "0", 0, "", false, 0, "")
+
 	}
 	// Better table
 	pdf.SetFont("Arial", "", 14)
 	pdf.AddPage()
 	encabezado()
 	basicTable()
-	err = pdf.OutputFileAndClose("./test/despacho"+fmt.Sprintf("%v",IDDespacho)+".pdf")
+	err = pdf.OutputFileAndClose("./test/despacho" + fmt.Sprintf("%v", IDDespacho) + ".pdf")
 	if err != nil {
 		fmt.Println("Error reading rows: " + err.Error())
 	}
-	f, err := os.Open("./test/despacho"+fmt.Sprintf("%v",IDDespacho)+".pdf")
+	f, err := os.Open("./test/despacho" + fmt.Sprintf("%v", IDDespacho) + ".pdf")
 	if err != nil {
 		fmt.Println(err)
 		w.WriteHeader(500)
